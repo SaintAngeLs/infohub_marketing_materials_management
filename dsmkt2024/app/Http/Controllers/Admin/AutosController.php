6fc6dbb55b5bdc55597ff265e83923f3c9845cf5
Andrii Voznesenskyi <01174367@pw.edu.pl>
2024-03-19 14:22:58 +0100

    (#1) adding the AdminPanel controllers and empty pages

---
+++ dsmkt2024/app/Http/Controllers/Admin/AutosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AutosController extends Controller
+{
+    public function index()
+    {
+        return view('autos.index');
+    }
+}
--- dsmkt2024/routes/web.php
+++ dsmkt2024/routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
+
+use App\Http\Controllers\Admin\AutosController;
+use App\Http\Controllers\Admin\ConsentionController;
+use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\StatisticsController;
+use App\Http\Controllers\Admin\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/consentions', [ConsentionController::class, 'index'])->name('consentions');
+
+Route::get('/users', [UsersController::class, 'index'])->name('users');
+
+Route::get('/autos', [AutosController::class, 'index'])->name('autos');
+
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+
+Route::get('/statistics', [StatisticsController::class, 'index'])->name('statistics');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
