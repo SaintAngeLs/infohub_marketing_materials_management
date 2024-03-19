cdb3ffed121683e6b4e44100ba40188c36d40785
Andrii Voznesenskyi <01174367@pw.edu.pl>
2024-03-19 11:34:38 +0100

    (#1) reset has

---
+++ dsmkt2024/database/migrations/2024_03_19_102527_add_remember_token_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Add remember_token column of type string, nullable
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Remove the column if the migration is rolled back
+            $table->dropColumn('remember_token');
+        });
+    }
+};
