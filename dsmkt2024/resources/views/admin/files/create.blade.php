2578f08bd8e28b2fa953225ceae1042d949a3e0f
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-03-27 12:19:54 +0100

    (#1) strategy pattern for saving the files defines with the source

--- dsmkt2024/resources/views/admin/files/create.blade.php
+++ dsmkt2024/resources/views/admin/files/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="">
+        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-900">
-                    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                        {{ __('Dodanie nowego pliku') }}
-                    </h2>
+                    <p class="content-tab-name">
+                        {{ __('Pliki / Dodaj nowy plik') }}
+                    <p class="content-tab-name">
 
                     @include('components.file-form-component.file-form-component', ['menuItemsToSelect' => $menuItemsToSelect])
                 </div>
--- dsmkt2024/resources/views/admin/menu/index.blade.php
+++ dsmkt2024/resources/views/admin/menu/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <div class="py-12">
+    <div class="">
         <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-900">
 
-                    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-900 leading-tight">
+                    <p class="content-tab-name">
                         {{ __('Struktura menu') }}
-                    </h2>
+                    </p>
                     <p  class="table-button">
                         <a href="{{ route('menu.create') }}" class="btn">Dodaj nową zakładkę</a>
                     </p>
