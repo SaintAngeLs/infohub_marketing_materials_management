039e98c8dc38cd1d797befd962f9653cde6ef034
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-03-28 18:01:46 +0100

    (#1) adding the UserGrpops controller and the create method update to pass the userids, updae the wouting in the web

---
+++ dsmkt2024/resources/views/admin/users/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+    <div class="">
+        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-100 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-900">
+
+                    <p class="content-tab-name">
+                        {{ __('Użytkownicy / Dodaj użytkownika') }}
+                    </p>
+                    <p  class="table-button">
+                        <a href="{{ route('user.create') }}" class="btn">Dodaj użyktownika</a>
+                    </p>
+
+                    @include('components.users-form-component.user-form-component')
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
