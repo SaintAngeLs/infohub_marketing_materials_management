c97bbb96f78be367821c63e43d52cb28edfc5b9a
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-04-05 13:52:39 +0200

    (#2) add the deploy pipeline

--- dsmkt2024/resources/js/menu_tree_user_notifications.js
+++ dsmkt2024/resources/js/menu_tree_user_notifications.js
@@ -18,40 +18,66 @@ $(document).ready(function() {
     }).on('ready.jstree', function (e, data) {
         updateNodePadding(data.instance);
         data.instance.open_all();
-    }).on('ready.jstree', function (e, data) {
-        updateNodePadding(data.instance);
-    }).on('open_node.jstree', function (e, data) {
-        updateNodePadding(data.instance, data.node.id);
+
+    // }).on('ready.jstree', function (e, data) {
+    //     updateNodePadding(data.instance);
+    // }).on('open_node.jstree', function (e, data) {
+    //     updateNodePadding(data.instance, data.node.id);
+    }).on('after_open.jstree', function(e, data) {
+        // Rebind the radio change event handler to ensure it captures dynamically loaded nodes
+
     });
 
-    $(document).on('change', '#menu-tree-permissions-user .menu-item-checkbox', function() {
-        var menuId = $(this).val();
-        var userId = $('#user-id').val();
-        console.log(userId);
-        var isChecked = $(this).is(':checked');
+    // $(document).on('change', '#menu-tree-permissions-user .menu-item-checkbox', function() {
+    //     var menuId = $(this).val();
+    //     var userId = $('#user-id').val();
+    //     console.log(userId);
+    //     var isChecked = $(this).is(':checked');
 
 
-        var action = isChecked ? 'assign' : 'remove';
+    //     var action = isChecked ? 'assign' : 'remove';
 
-        $.ajax({
-            url: '/menu/permissions/update-user-permission',
-            method: 'POST',
-            data: {
-                menu_id: menuId,
-                user_id: userId,
-                action: action,
-                _token: $('meta[name="csrf-token"]').attr('content')
-            },
-            success: function(response) {
-                alert('Permissions updated successfully.');
-            },
-            error: function(xhr, status, error) {
-                console.error('Error updating permissions:', error);
-            }
-        });
-    });
+    //     $.ajax({
+    //         url: '/menu/permissions/update-user-permission',
+    //         method: 'POST',
+    //         data: {
+    //             menu_id: menuId,
+    //             user_id: userId,
+    //             action: action,
+    //             _token: $('meta[name="csrf-token"]').attr('content')
+    //         },
+    //         success: function(response) {
+    //             alert('Permissions updated successfully.');
+    //         },
+    //         error: function(xhr, status, error) {
+    //             console.error('Error updating permissions:', error);
+    //         }
+    //     });
+    // });
 
-    $(document).on('change', '.notification-preferences input[type="radio"]', function() {
+    // $('#menu-tree-notifications').on('change', '.notification-preferences input[type="radio"]', function() {
+    //     var menuItemId = $(this).closest('.js-tree-node-content').data('node-id');
+    //     var frequency = $(this).val();
+
+    //     $.ajax({
+    //         url: '/user/update-menu-item-notification',
+    //         method: 'POST',
+    //         data: {
+    //             menu_item_id: menuItemId,
+    //             frequency: frequency,
+    //             _token: $('meta[name="csrf-token"]').attr('content')
+    //         },
+    //         success: function(response) {
+    //             alert('Notification preference updated successfully.');
+    //         },
+    //         error: function(xhr, status, error) {
+    //             console.error('Error updating notification preference:', error);
+    //         }
+    //     });
+    // });
+
+
+    $('#menu-tree-notifications').on('change', '.notification-preferences input[type="radio"]', function() {
         var menuItemId = $(this).closest('.js-tree-node-content').data('node-id');
         var frequency = $(this).val();
 
@@ -73,6 +99,10 @@ $(document).ready(function() {
     });
 
 
+    // Immediately invoke to bind event to any existing nodes
+    attachRadioChangeEvent();
+
+
     function updateNodePadding(instance, nodeId) {
         var selector = nodeId ? '#' + nodeId + ' > .jstree-children' : '#menu-tree .jstree-node';
         $(selector).each(function() {
@@ -84,7 +114,7 @@ $(document).ready(function() {
 });
 
 $(document).on('click', '.node-name', function(e) {
-    e.stopPropagation();
+    // e.stopPropagation();
     var nodeId = $(this).closest('.jstree-node').attr('id');
     window.location.href = '/menu/edit/' + nodeId;
 });
