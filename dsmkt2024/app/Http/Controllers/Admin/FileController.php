d1d29cbac0d3fb07b0cdabcf240f24fb56997333
SaintAngeLs <voznesenskijandrej5@gmail.com>
2024-06-20 01:31:55 +0200

    #30 update the zip file download from the user menu

--- dsmkt2024/app/Http/Controllers/Admin/FileController.php
+++ dsmkt2024/app/Http/Controllers/Admin/FileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use ZipArchive;
 use App\Contracts\IStatistics;
 use App\Http\Controllers\Controller;
 use App\Models\Auto;
@@ -277,7 +278,13 @@ class FileController extends Controller
         $this->statisticsService->logDownload(auth()->id(), $fileId);
 
         return response()->download($filePath, basename($filePath), [
-            'Content-Type' => mime_content_type($filePath)
+            'Content-Type' => mime_content_type($filePath),
+            'Content-Disposition' => 'attachment; filename="'.basename($filePath).'"',
+            'Content-Transfer-Encoding' => 'binary',
+            'Expires' => '0',
+            'Cache-Control' => 'must-revalidate',
+            'Pragma' => 'public',
+            'Content-Length' => filesize($filePath),
         ]);
     }
 
@@ -352,6 +359,70 @@ class FileController extends Controller
 
         return $hasChanges;
     }
+
+    public function downloadMultiple(Request $request)
+    {
+        $fileIds = $request->input('files');
+        if (empty($fileIds)) {
+            return redirect()->back()->with('error', 'No files selected.');
+        }
+
+        $files = File::whereIn('id', $fileIds)->get();
+        if ($files->isEmpty()) {
+            return redirect()->back()->with('error', 'No files found.');
+        }
+
+        $zip = new ZipArchive;
+        $zipFileName = 'downloads_' . time() . '.zip';
+        $zipStoragePath = storage_path('app/public/' . $zipFileName);
+        $zipPublicDir = public_path('downloads');
+        $zipPublicPath = $zipPublicDir . '/' . $zipFileName;
+
+        // Ensure the public downloads directory exists
+        if (!file_exists($zipPublicDir)) {
+            mkdir($zipPublicDir, 0777, true);
+        }
+
+        Log::info('Attempting to create ZIP file at: ' . $zipStoragePath);
+
+        if ($zip->open($zipStoragePath, ZipArchive::CREATE) === TRUE) {
+            foreach ($files as $file) {
+                $filePath = storage_path('app/public/' . $file->path);
+                if (file_exists($filePath)) {
+                    $zip->addFile($filePath, basename($filePath));
+                    Log::info('Added file to ZIP: ' . $filePath);
+                } else {
+                    Log::warning('File not found: ' . $filePath);
+                }
+            }
+
+            $zip->close();
+            Log::info('ZIP file created at: ' . $zipStoragePath);
+
+            if (file_exists($zipStoragePath) && filesize($zipStoragePath) > 0) {
+                if (copy($zipStoragePath, $zipPublicPath)) {
+                    unlink($zipStoragePath); // Remove the file from storage path after moving
+                    Log::info('ZIP file moved to public path: ' . $zipPublicPath);
+                    return response()->download($zipPublicPath)->deleteFileAfterSend(true);
+                } else {
+                    Log::error('Failed to move ZIP file from ' . $zipStoragePath . ' to ' . $zipPublicPath);
+                    return redirect()->back()->with('error', 'Failed to move ZIP file.');
+                }
+            } else {
+                Log::error('ZIP file does not exist or is empty: ' . $zipStoragePath);
+                return redirect()->back()->with('error', 'ZIP file creation failed or ZIP file is empty.');
+            }
+        } else {
+            Log::error('Failed to create ZIP file: ' . $zipStoragePath);
+            return redirect()->back()->with('error', 'Failed to create ZIP file.');
+        }
+    }
+
+
+
+
+
+
 
 
 
--- dsmkt2024/resources/views/user/menu/index.blade.php
+++ dsmkt2024/resources/views/user/menu/index.blade.php
@@ -8,65 +8,56 @@
                     <p class="content-tab-name">
                         {{ __('Menu') }}
                     </p>
-                    {{-- @forelse($menuItems as $menuItem)
-                    <h3>Files for {{ $menuItem->name }}</h3>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Nazwa</th>
-                                <th>Typ</th>
-                                <th>Rozmiar</th>
-                                <th>Aktualizacja</th>
-                                <th>Pobierz</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($menuItem->files as $file)
-                                <tr>
-                                    <td>{{ $file->name }}</td>
-                                    <td>{{ $file->extension }}</td>
-                                    <td>{{  \App\Helpers\FormatBytes::formatBytes($file->weight) }}</td>
-                                    <td>{{ $file->updated_at->format('d.m.Y H:i:s') }}</td>
-                                    <td><a href="{{ route('files.download', $file->id) }}">Pobierz</a></td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @empty
-                    <li>Brak plików</li>
-                @endforelse --}}
 
-                @if(isset($selectedMenuItem))
-                    <h3>Files for {{ $selectedMenuItem->name }}</h3>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Nazwa</th>
-                                <th>Typ</th>
-                                <th>Rozmiar</th>
-                                <th>Aktualizacja</th>
-                                <th>Pobierz</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($selectedMenuItem->files as $file)
+                    @if(isset($selectedMenuItem))
+                        <h3>Files for {{ $selectedMenuItem->name }}</h3>
+                        <form id="download-form" method="POST" action="{{ route('files.downloadMultiple') }}">
+                            @csrf
+                            <table class="table">
+                                <thead>
                                 <tr>
-                                    <td>{{ $file->name }}</td>
-                                    <td>{{ $file->extension }}</td>
-                                    <td>{{  \App\Helpers\FormatBytes::formatBytes($file->weight) }}</td>
-                                    <td>{{ $file->updated_at->format('d.m.Y H:i:s') }}</td>
-                                    <td><a href="{{ route('files.download', $file->id) }}">Download</a></td>
+                                    <th>Select</th>
+                                    <th>Name</th>
+                                    <th>Type</th>
+                                    <th>Size</th>
+                                    <th>Updated</th>
+                                    <th>Download</th>
                                 </tr>
-                            @empty
-                                <tr><td colspan="5">Brak plików</td></tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                @else
-                    <p>No menu item selected or does not exist.</p>
-                @endif
+                                </thead>
+                                <tbody>
+                                @forelse ($selectedMenuItem->files as $file)
+                                    <tr>
+                                        <td><input type="checkbox" name="files[]" value="{{ $file->id }}"></td>
+                                        <td>{{ $file->name }}</td>
+                                        <td>{{ $file->extension }}</td>
+                                        <td>{{ \App\Helpers\FormatBytes::formatBytes($file->weight) }}</td>
+                                        <td>{{ $file->updated_at->format('d.m.Y H:i:s') }}</td>
+                                        <td><a href="{{ route('files.download', $file->id) }}">Download</a></td>
+                                    </tr>
+                                @empty
+                                    <tr><td colspan="6">No files available</td></tr>
+                                @endforelse
+                                </tbody>
+                            </table>
+                            <button type="button" onclick="downloadSelectedFiles()">Download Selected</button>
+                        </form>
+                    @else
+                        <p>No menu item selected or does not exist.</p>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        function downloadSelectedFiles() {
+            var form = document.getElementById('download-form');
+            var checkboxes = form.querySelectorAll('input[type="checkbox"]:checked');
+            if (checkboxes.length > 0) {
+                form.submit();
+            } else {
+                alert('Please select at least one file to download.');
+            }
+        }
+    </script>
 @endsection
