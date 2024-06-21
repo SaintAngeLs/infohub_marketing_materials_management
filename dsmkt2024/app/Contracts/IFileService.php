f071518afd98c1ab14a0c192947ca15aff3d03d2
SaintAngeLs <voznesenskijandrej5@gmail.com>
2024-06-21 22:00:43 +0200

    #51 refactoring the files service adding the IFileService interface

---
+++ dsmkt2024/app/Contracts/IFileService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+
+interface IFileService
+{
+    public function getMenuItemsToSelect();
+    public function getAutos();
+    public function getServerFiles();
+    public function validateRequest(Request $request, $isStore = true);
+    public function handleFileUpload(Request $request, File &$file, array $validated);
+    public function updateFileModel(File &$file, array $validated);
+    public function deleteFile(File $file);
+    public function downloadFile(File $file);
+    public function toggleStatus(File $file);
+    public function detectFileChanges(File $file, array $validated): bool;
+    public function downloadMultipleFiles(Request $request);
+}
