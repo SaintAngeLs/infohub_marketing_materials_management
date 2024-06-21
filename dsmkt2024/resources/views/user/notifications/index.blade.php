e9e72812c04205b1fd867d51052b785b649d3016
SaintAngeLs <voznesenskijandrej5@gmail.com>
2024-06-21 20:00:59 +0200

    #45 update the buttons styles for the notification preferences

--- dsmkt2024/resources/views/user/notifications/index.blade.php
+++ dsmkt2024/resources/views/user/notifications/index.blade.php
@@ -24,14 +24,14 @@
     <div class="container mx-auto sm:px-6 lg:px-8">
         <div class="bg-white shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
-                <p class="text-lg font-semibold">{{ __('My Account / Email Notification Changes') }}</p>
+                <p class="text-lg font-semibold">{{ __('Moje konto / Ustawienia powiadomień o zmianach') }}</p>
                 <table class="table-auto w-full mt-4">
                     <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Never</th>
-                        <th>Daily</th>
-                        <th>On Change</th>
+                        <th>Nazwa zakładki</th>
+                        <th>Nigdy</th>
+                        <th>Każdego dnia</th>
+                        <th>Przy kadej zmianie</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -41,8 +41,12 @@
                     </tbody>
                 </table>
                 <div class="mt-4 flex justify-end">
-                    <button class="btn btn-primary">{{ __('Save Changes') }}</button>
-                    <a href="{{ url()->previous() }}" class="btn btn-secondary ml-2">{{ __('Cancel') }}</a>
+                    <div class="table-button">
+                        <a href="#" onclick="event.preventDefault(); this.closest('form').submit();" class="btn">{{ __('Zapisz zmiany') }}</a>
+                    </div>
+                    <div class="table-button-2 ml-2">
+                        <a href="{{ url()->previous() }}" class="btn">{{ __('Anuluj') }}</a>
+                    </div>
                 </div>
             </div>
         </div>
